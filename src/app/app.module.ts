e8d99908c7cd63ebf0f178fbe9358c8d6c636d40
SKT.배기도 <skt.1111489@sk.com>
2019-01-13 19:31:28 +0900

    split component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { RecommendedComponent } from './recommended/recommended.component';
+import { NewsongComponent } from './newsong/newsong.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    RecommendedComponent,
+    NewsongComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/newsong/newsong.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newsong',
+  templateUrl: './newsong.component.html',
+  styleUrls: ['./newsong.component.css']
+})
+export class NewsongComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
